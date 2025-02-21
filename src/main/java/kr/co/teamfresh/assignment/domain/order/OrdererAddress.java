a6517537983a609e245e14ba1b6ffc5d4122ec0a
kgggh <68227318+kgggh@users.noreply.github.com>
2025-02-21 22:53:19 +0900

    refactor: 주문지 VO 생성자 추가

--- src/main/java/kr/co/teamfresh/assignment/domain/order/OrdererAddress.java
+++ src/main/java/kr/co/teamfresh/assignment/domain/order/OrdererAddress.java
@@ -1,8 +1,13 @@
 package kr.co.teamfresh.assignment.domain.order;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class OrdererAddress {
     private String address;
 }
